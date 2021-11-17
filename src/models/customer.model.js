c2664851edd1eace41eb51c9cb638625f6290f5b
Sebastien Beaudet <1532686@cstj.qc.ca>
2021-11-17 10:12:49 -0500

    Fin model customer

--- src/models/customer.model.js
+++ src/models/customer.model.js
@@ -1,20 +1,20 @@
 import mongoose from 'mongoose';
-import PLANET_NAMES from '../libs/constants.js';
+import {PLANET_NAMES} from '../libs/constants.js';
 
 const customerSchema = mongoose.Schema({
     name:{type: String, required:true},
     email:{type: String, unique:true, required:true},
-    planet:{type:String, enum:PLANET_NAMES},    //TODO: Valider que enum est bien appelé (probablement pas)
+    planet:{type:String, enum:PLANET_NAMES},
     coord:{
         lat:{type:Number, min:-1000, max:1000, required:true},
         lon:{type:Number, min:-1000, max:1000, required:true}
     },
-    phone:{type:String, required:true}, //TODO: Valider si type hexa existe
-    birthday:{type:Date,required:true}, //TODO: Valider si doit être à now par défaut
+    phone:{type:String, required:true},
+    birthday:{type:Date,required:true},
     referalCode:{type:String}
 },{
     collection:'customers',
     strict:'throw'
 });
 
-export default mongoose.model('Customers', customerSchema);
+export default mongoose.model('Customer', customerSchema);
